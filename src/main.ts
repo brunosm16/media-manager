19a35b58fb37ecf3e326a09c859a14b16897bbcc
brunosm16 <brunoskr23@gmail.com>
2024-02-29 13:18:32 -0300

    feat: 🚀 use NestExpressApplication

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+import type { NestExpressApplication } from '@nestjs/platform-express';
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy');
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
